4705e4fa30bf46da3547e05a4b3ab24c7bb30b3a
Darya Suravtsova <darblnkapm@gmail.com>
2022-11-10 13:43:50 +0300

    feat: add solution for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const way = path.join(__dirname, 'text.txt')
+
+const fs = require('fs')
+const recordedStream = fs.createWriteStream(way)
+
+const { stdout, stdin } = require('process')
+const readline = require('readline').createInterface(stdin, stdout)
+
+console.log('Введите текст для записи в файл')
+
+readline.on('line', (data) => {
+  if (data.toLowerCase() !== '.exit') {
+    recordedStream.write(data)
+    console.log('Что-нибудь еще?')
+  }
+  else {
+    console.log('Пока!')
+    recordedStream.close()
+    readline.close()
+  }
+})
+
+readline.on('SIGINT', () => {
+  console.log('Пока!')
+  recordedStream.close()
+  readline.close()
+})
