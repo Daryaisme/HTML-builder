83e555a365d865c58ea1210ffe023cf34723133a
Darya Suravtsova <darblnkapm@gmail.com>
2022-11-10 20:40:07 +0300

    feat: add solution of 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+const wayOrigin = path.join(__dirname, 'styles')
+const wayCopy = path.join(__dirname, 'project-dist')
+
+const fs = require('fs')
+fs.readdir(wayOrigin, {withFileTypes: true}, (err, files) => {
+  if (!err) {
+    files.map(file => {
+      if (file.isFile() && file.name.slice(file.name.indexOf('.'), file.name.length) == '.css') {
+        const readableStream = fs.createReadStream(path.join(wayOrigin, file.name), 'utf-8');
+        readableStream.on('data', (data, err) => {
+          if (!err) {
+            fs.appendFile(path.join(wayCopy, 'bundle.css'), data, () => {})
+          }
+          else throw new Error()
+        })
+      }
+    })
+  }
+  else throw new Error()
+})
