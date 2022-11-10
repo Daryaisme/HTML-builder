f2f81aeee0e7f8c340d0dc42a55e15697990b129
Darya Suravtsova <darblnkapm@gmail.com>
2022-11-10 21:19:59 +0300

    fix: change solution for 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,7 +4,13 @@ const wayOrigin = path.join(__dirname, 'styles')
 const wayCopy = path.join(__dirname, 'project-dist')
 
 const fs = require('fs')
+
 fs.readdir(wayOrigin, {withFileTypes: true}, (err, files) => {
+  fs.access(path.join(wayCopy, 'bundle.css'), (err) => {
+    if (!err) fs.unlink(path.join(wayCopy, 'bundle.css'), (err) => {
+      if (err) throw new Error()
+    }) 
+  })
   if (!err) {
     files.map(file => {
       if (file.isFile() && file.name.slice(file.name.indexOf('.'), file.name.length) == '.css') {
