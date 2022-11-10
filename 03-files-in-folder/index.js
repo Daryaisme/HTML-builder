80bb94311431acfc877edc681893f4bb20e96000
Darya Suravtsova <darblnkapm@gmail.com>
2022-11-10 21:40:59 +0300

    refactor: indexOf replaced by lastIndexOf in 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,7 +9,7 @@ fs.readdir(way, {withFileTypes : true}, (err, files) => {
         newWay = path.join(__dirname, 'secret-folder', file.name)
         fs.stat(newWay, {withFileTypes : true}, (err, stats) => {
           if (!err) {
-            console.log(file.name.slice(0, file.name.indexOf('.')), '-', file.name.slice(file.name.indexOf('.') + 1, file.name.length), '-', stats.size, 'bytes')
+            console.log(file.name.slice(0, file.name.lastIndexOf('.')), '-', file.name.slice(file.name.lastIndexOf('.') + 1, file.name.length), '-', stats.size, 'bytes')
           }
         })
       }
